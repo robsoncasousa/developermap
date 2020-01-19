f55c881a730d0933d519add022114ca7ccd3cfb1
Robson Sousa <robsoncasousa@gmail.com>
2020-01-19 14:05:44 -0300

    adapting api to receive external connections

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://robson:robson@cluster0-epjw8.mongodb.net/develop
     useUnifiedTopology: true
 });
 
+app.use(cors())
 app.use(express.json()); //precisa vir antes das rotas
 app.use(routes);
 
