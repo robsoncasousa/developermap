3ef58257f16c735ff056801c8fdee43630aa6bd6
Robson Sousa <robsoncasousa@gmail.com>
2020-01-26 00:38:58 -0300

    mobile module finished

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-export default function App() {
+import Routes from './src/routes';
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Robson Cavalcante</Text>
-    </View>
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7d40e7'/>
+      <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color:'#fff',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-});
+export default App;
